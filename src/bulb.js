d353fa8bf9a9a5c4092f7bad916d82774fdd3e06
KiranRaj27 <92741173+KiranRaj27@users.noreply.github.com>
2022-01-23 19:08:41 +0530

    image

--- src/bulb.js
+++ src/bulb.js
@@ -2,28 +2,38 @@ import React, { useState } from "react";
 import "./bulb.css";
 
 const Bulb = () => {
-  const [color, setColor] = useState("grey");
+  const [color, setColor] = useState(
+    "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg"
+  );
   const [line1, setLine1] = useState("OFF");
 
-  const [color2, setColor2] = useState("grey");
+  const [color2, setColor2] = useState(
+    "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg"
+  );
   const [line2, setLine2] = useState("OFF");
 
   const change = () => {
     if (line1 === "OFF") {
-      setColor("orange");
+      setColor("https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg");
       setLine1("ON");
     } else {
-      setColor("grey");
+      setColor(
+        "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg"
+      );
       setLine1("OFF");
     }
   };
 
   const change2 = () => {
     if (line2 === "OFF") {
-      setColor2("orange");
+      setColor2(
+        "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg"
+      );
       setLine2("ON");
     } else {
-      setColor2("grey");
+      setColor2(
+        "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg"
+      );
       setLine2("OFF");
     }
   };
@@ -31,13 +41,19 @@ const Bulb = () => {
   return (
     <>
       <div className="wrap">
-        <div className="bulb" style={{ backgroundColor: color }}></div>
+        <div
+          className="bulb"
+          style={{ backgroundImage: `url(${color})` }}
+        ></div>
         <button className="switch" onClick={change}>
           {line1}
         </button>
       </div>
       <div className="wrap">
-        <div className="bulb" style={{ backgroundColor: color2 }}></div>
+        <div
+          className="bulb"
+          style={{ backgroundImage: `url(${color2})` }}
+        ></div>
         <button className="switch" onClick={change2}>
           {line2}
         </button>
